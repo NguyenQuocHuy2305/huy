50e3f12db9d2d55d0763c5ee8a4da47087d92120
NguyenQuocHuy2305 <106923317+NguyenQuocHuy2305@users.noreply.github.com>
2022-06-22 00:01:22 +0700

    bithub

---
+++ nguyenquochuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_nguyenquohuy.Startup))]
+namespace NguyenQuocHuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
